718b88d33faac93391d9772f99dd6b88e074b504
Vimal Sharma <vimalsharma10082000@gmail.com>
2025-05-13 11:02:01 +0530

    Implement authentication middleware and user registration with email verification

---
+++ src/app/api/sign-up/route.ts
@@ -0,0 +1,93 @@
+import { sendVerificationEmail } from "@/helpers/sendVerificationEmail";
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/model/User";
+import bcrypt from "bcryptjs";
+// import VerificationEmail from "../../../../emails/verificationEmail";
+
+export async function POST(request: Request) {
+  await dbConnect();
+  try {
+    const { username, email, password } = await request.json();
+    const existingUserVerifiedByUsername = await UserModel.findOne({
+      username,
+      isverified: true,
+    });
+    if (existingUserVerifiedByUsername) {
+      return Response.json(
+        {
+          success: false,
+          message: "Username is already taken",
+        },
+        { status: 400 }
+      );
+    }
+    const existingUserByEmail = await UserModel.findOne({ email });
+    const verifyCode = Math.floor(100000 + Math.random() * 900000).toString();
+    if (existingUserByEmail) {
+      if (existingUserByEmail.isVerified) {
+        return Response.json(
+          {
+            success: false,
+            message: "User already exist with this email",
+          },
+          { status: 500 }
+        );
+      } else {
+        const hashedPassword = await bcrypt.hash(password, 10);
+        existingUserByEmail.password = hashedPassword;
+        existingUserByEmail.verifyCode = verifyCode;
+        existingUserByEmail.verifyCodeExpiry = new Date(Date.now() + 3600000);
+        await existingUserByEmail.save();
+      }
+    } else {
+      const hashedPassword = await bcrypt.hash(password, 10);
+      const expiryDate = new Date();
+      expiryDate.setHours(expiryDate.getHours() + 1);
+      const newUser = new UserModel({
+        username: username,
+        email: email,
+        password: hashedPassword,
+        verifyCode: verifyCode,
+        verifyCodeExpiry: expiryDate,
+        isverified: false,
+        isAcceptingMessage: true,
+        message: [],
+      });
+      await newUser.save();
+    }
+    //Send Verification Email
+
+    const emailResponse = await sendVerificationEmail(
+      email,
+      username,
+      verifyCode
+    );
+    if (!emailResponse.success) {
+      return Response.json(
+        {
+          success: false,
+          message: emailResponse.message,
+        },
+        { status: 500 }
+      );
+    }
+    return Response.json(
+      {
+        success: true,
+        message: "User Registered Successfully , Please verify your Email",
+      },
+      { status: 201 }
+    );
+  } catch (error) {
+    console.error("Error registering user", error);
+    return Response.json(
+      {
+        success: false,
+        message: "Error Regstering User",
+      },
+      {
+        status: 500,
+      }
+    );
+  }
+}
--- src/model/User.ts
+++ src/model/User.ts
@@ -23,7 +23,7 @@ export interface User extends Document {
   password: string;
   verifyCode: string;
   verifyCodeExpiry: Date;
-  isverified: boolean;
+  isVerified: boolean;
   isAcceptingMessage: boolean;
   message: Message[];
 }
@@ -55,7 +55,7 @@ const UserSchema: Schema<User> = new Schema({
     type: Date,
     required: [true, "Verify Code Expiry is required"],
   },
-  isverified: {
+  isVerified: {
     type: Boolean,
     default: false,
   },
